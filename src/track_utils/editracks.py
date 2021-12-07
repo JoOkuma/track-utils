4d819b86502be77bcad1d495923e6230f390776f
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2021-12-07 11:11:19 -0800

    added neighborhood display

--- src/track_utils/editracks.py
+++ src/track_utils/editracks.py
@@ -1,7 +1,9 @@
 
+from sre_compile import dis
 from typing import Container, Optional
 
 import numpy as np
+from numpy.core.fromnumeric import argsort
 from numpy.typing import ArrayLike
 import napari
 
@@ -18,7 +20,6 @@ from vispy.visuals.filters import Filter
 class SpatialFilter(Filter):
 
     VERT_SHADER = """
-        // varying vec4 v_spatial_color;
         void apply_track_spatial_shading()
         {
             float relative_dif = abs($a_vertex_stack - $current_stack) / $stack_range;
@@ -30,7 +31,6 @@ class SpatialFilter(Filter):
     """
 
     FRAG_SHADER = """
-        // varying vec4 v_spatial_color;
         void apply_track_spatial_shading()
         {
             // if the alpha is below the threshold, discard the fragment
@@ -92,6 +92,7 @@ class EditTracks(Container):
 
         self._hover_visual = Markers(scaling=True)
         self._eval_visual = Markers(scaling=True)
+        self._neighbor_visual = Markers(scaling=True)
         self._spatial_filter = SpatialFilter(10.0)
 
         self._node_radius = 10
@@ -99,18 +100,22 @@ class EditTracks(Container):
         self._node_spatial_trail_length = 10
 
         self._reset_eval_tracking()
+        self._reset_neighborhood()
 
         self._load_button = PushButton(text='Load', enabled=False)
         self._load_button.changed.connect(self._on_load)
         self.append(self._load_button)
 
-        self._node_radius = SpinBox(max=100, value=10, label='Radius')
+        self._node_radius = SpinBox(max=100, value=10, label='Node Radius')
         self.append(self._node_radius)
 
         self._spatial_range = Slider(min=0, max=500, value=self._spatial_filter.stack_range,
                                      label='Spatial Range')
         self._spatial_range.changed.connect(lambda e: setattr(self._spatial_filter, 'stack_range', e.value))
         self.append(self._spatial_range)
+
+        self._neighbor_radius = SpinBox(min=1, max=25, value=10, label='Neigh. Radius')
+        self.append(self._neighbor_radius)
 
         self._tracks_layer.changed.connect(lambda v: setattr(self._load_button, 'enabled', v is not None))
         self._viewer.layers.events.removed.connect(self._on_layer_removed)
@@ -167,6 +172,7 @@ class EditTracks(Container):
 
         coord = layer.world_to_data(np.array(event.position))
         index = self._get_index(layer, coord)
+
         if index is None:
             pass
 
@@ -200,20 +206,7 @@ class EditTracks(Container):
 
         else:
             print('drag option not found')
-        
-        """
-        elif different modes ...
 
-        with clicking:
-            MANUAL LINK MODE manually pick the next cell (must display both)
-        
-        with just an action:
-            CYCLE LINK MODE might be a better option,  show candidates, otherwise user must add an cell
-
-        other modes to think about:
-            to add an node, or multiple nodes sequentially
-        """
-    
     def _select_node(self, index: int) -> None:
         self._inspecting = False
         self._make_empty(self._hover_visual)
@@ -228,7 +221,6 @@ class EditTracks(Container):
         self._set_eval_markers()
 
     def _set_eval_markers(self, t_radius: int = 0) -> None:
-        # TODO add color to indicate forward and backward nodes
         index = self._eval_relative_id
         self._center_to(self._eval_track[index])
 
@@ -249,6 +241,9 @@ class EditTracks(Container):
             edge_width=self._node_edge_width,
         )
         self._node.update()
+
+        next_coord = self._eval_track[self._eval_relative_id] + np.array([1, 0, 0, 0])
+        self._load_neighborhood(next_coord, self._neighbor_radius.value)
     
     @property
     def _node_edge_width(self) -> float:
@@ -264,8 +259,8 @@ class EditTracks(Container):
         visual.set_data(np.empty((0, 3)))
       
     def _escape_eval_mode(self) -> None:
-        self._make_empty(self._eval_visual)
         self._reset_eval_tracking()
+        self._reset_neighborhood()
 
     def _reset_eval_tracking(self) -> None:
         self._inspecting = True
@@ -273,6 +268,66 @@ class EditTracks(Container):
         self._eval_track = None
         self._eval_original_id = None
         self._eval_relative_id = None
+        self._make_empty(self._eval_visual)
+
+    def _load_neighborhood(self, coord: ArrayLike, radius: int) -> None:
+        layer: Tracks = self._tracks_layer.value
+        if layer is None:
+            return
+
+        coord = np.array(coord)
+
+        selected, = np.nonzero(np.abs(layer._manager.track_times - coord[0]) < 1)
+
+        if len(selected) == 0:
+            self._reset_neighborhood()
+            return
+
+        track_vertices = layer._manager.track_vertices[selected]
+        distances = np.linalg.norm(track_vertices[:, 1:] - coord[np.newaxis, 1:], axis=1)
+        
+        neighbors_mask = distances < radius
+        distances = distances[neighbors_mask]
+        selected = selected[neighbors_mask]
+        if len(selected) == 0:
+            self._reset_neighborhood()
+            return
+
+        indices = np.argsort(distances)
+        selected = selected[indices]
+
+        self._neighbor_original_ids = selected
+        self._neighbor_selected_id = 0
+        self._neighbor_vertices = layer._manager.track_vertices[selected]
+
+        self._load_neighborhood_visual()
+    
+    def _load_neighborhood_visual(self) -> None:
+        edge_color = np.empty((len(self._neighbor_vertices), 4), dtype=np.float32)
+        edge_color[...] = _handle_str('yellow')[np.newaxis, ...]
+        edge_color[self._neighbor_selected_id] = _handle_str('magenta')
+
+        self._neighbor_visual.set_data(
+            self._to_display(self._neighbor_vertices[:, 1:]),
+            face_color='transparent',
+            edge_color=edge_color,
+            size=self._node_radius.value,
+            edge_width=self._node_edge_width,
+        )
+        self._node.update()
+
+    def _increment_neighbor_selected_index(self, step: int) -> None:
+        if self._neighbor_selected_id is None:
+            return
+        
+        self._neighbor_selected_id = (self._neighbor_selected_id + step) % len(self._neighbor_vertices)
+        self._load_neighborhood_visual()
+ 
+    def _reset_neighborhood(self) -> None:
+        self._neighbor_original_ids = None
+        self._neighbor_selected_id = None
+        self._neighbor_vertices = None
+        self._make_empty(self._neighbor_visual)
     
     def _get_index(self, layer: Tracks, coord: ArrayLike, radius: int = 5) -> Optional[int]:
         coord = np.array(coord)
@@ -330,17 +385,19 @@ class EditTracks(Container):
         
         self._on_tracks_change()
 
+        aux_visuals = [self._hover_visual, self._eval_visual, self._neighbor_visual]
         visual = self._viewer.window.qt_viewer.layer_to_visual[layer]
         self._node = visual.node
-        for visual in (self._hover_visual, self._eval_visual):
+        for visual in aux_visuals:
             try:
                 self._node.remove_subvisual(visual)
             except ValueError:
                 pass
 
+        # visuals must be removed before attaching spatial filter
         self._node.attach(self._spatial_filter)
-        self._node.add_subvisual(self._hover_visual)
-        self._node.add_subvisual(self._eval_visual)
+        for visual in aux_visuals:
+            self._node.add_subvisual(visual)
 
         layer.events.rebuild_tracks.connect(self._on_tracks_change)
         self._spatial_range.changed.connect(lambda _: layer.refresh())
@@ -351,6 +408,10 @@ class EditTracks(Container):
 
         layer.bind_key('N', lambda _: self._increment_eval_relative_index(-1), overwrite=True)
         layer.bind_key('M', lambda _: self._increment_eval_relative_index(+1), overwrite=True)
+
+        layer.bind_key('K', lambda _: self._increment_neighbor_selected_index(-1), overwrite=True)
+        layer.bind_key('L', lambda _: self._increment_neighbor_selected_index(+1), overwrite=True)
+
         layer.bind_key('D', lambda _: self._delete_eval_node(), overwrite=True)
         layer.bind_key('Escape', lambda _: self._escape_eval_mode(), overwrite=True)
          
@@ -359,6 +420,7 @@ class EditTracks(Container):
             visual = self._viewer.window.qt_viewer.layer_to_visual[layer]
             visual.node.remove_subvisual(self._hover_visual)
             visual.node.remove_subvisual(self._eval_visual)
+            visual.node.remove_subvisual(self._neighbor_visual)
             visual.detach(self._spatial_filter)
     
     def _on_tracks_change(self) -> None:
@@ -366,6 +428,7 @@ class EditTracks(Container):
         self._update_current_slice()
         
     def _set_filter_stack(self) -> None:
+        print('track change')
         vertices = self._tracks_layer.value._manager.track_vertices
         self._spatial_filter.vertex_stack = vertices[:, self._non_visible_spatial_axis()]
     
@@ -381,9 +444,15 @@ class EditTracks(Container):
         if event.value == 3:
             self._spatial_filter.stack_range = 0
             self._hover_visual.visible = False
+            if self._neighbor_original_ids is None:
+                self._neighbor_visual.visible = False
         else:
             self._spatial_filter.stack_range = self._spatial_range.value
             self._hover_visual.visible = True
+            self._neighbor_visual.visible = True
+        
+        self._set_eval_markers()
+        self._load_neighborhood_visual()
    
     def _delete_eval_node(self) -> None:
         layer: Tracks = self._tracks_layer.value
